9a77c92f5374d5f678a0345911dc39d30b8b402b
Junichi Sasaki <junichi200123@gmail.com>
2023-05-04 22:57:20 +0900

    check overflow/underflow in atoi_base.c

--- atoi_base.c
+++ atoi_base.c
@@ -42,13 +42,35 @@ static bool is_valid_base(char *base) {
 }
 
 static char *skip_spaces(char *str) {
-  while (*str && (*str <= 32 || *str == 127)) {
+  while (*str) {
+    if (*str > 32 && *str != 127) break;
+    str++;
+  }
+  return str;
+}
+
+static char *parse_sign(char *str, int *sign){
+  *sign = 1;
+  while (*str) {
+    // if (*str != '+' && *str != '-') {
+    //   break;
+    // }
+    if (*str < '+') break;
+    if (*str > '-') break;
+    if (*str == ',') break;
+    if (*str == '-') {
+      *sign = -1;
+    }
     str++;
   }
   return str;
 }
 
 int ft_atoi_base(char *str, char *base) {
+  int sign;
+  int base_len;
+  int num;
+  int idx;
   // validate
   if (str == NULL || !is_valid_base(base)) {
     return 0;
@@ -56,25 +78,26 @@ int ft_atoi_base(char *str, char *base) {
   // skip spaces
   str = skip_spaces(str);
   // parse sign
-  int sign = 1;
-  while (*str && (*str == '+' || *str == '-')) {
-    if (*str == '-') {
-      sign = -1;
-    }
-    str++;
-  }
+  str = parse_sign(str, &sign);
   // parse number
-  int base_len = strlen(base);
-  int num = 0;
-  int idx;
-  while (*str && (idx = ft_strchr(base, *str)) != -1) {
-    if ((sign > 0 && num > (INT_MAX - idx) / base_len) || (sign < 0 && -1 * num < (INT_MIN + idx) / base_len)) {
-      return 0;
+  base_len = strlen(base);
+  num = 0;
+  while (*str) {
+    idx = ft_strchr(base, *str);
+    if (idx == -1) break;
+    if (sign > 0) {
+      if (num > (INT_MAX - idx) / base_len) {
+        return 0;
+      }
+    }
+    if (sign < 0){
+      if (-1 * num < (INT_MIN + idx) / base_len) {
+        return 0;
+      }
     }
     num = num * base_len + idx;
     str++;
   }
-  // TODO: overflow and underflow
   return sign * num;
 }
 
@@ -88,6 +111,7 @@ int		main(void)
 	printf("%d\n", ft_atoi_base(" 	+---539", "0123456789abcdef"));
 	printf("%d\n", ft_atoi_base(" 	+539", "0123456789abcdef"));
 	printf("%d\n", ft_atoi_base(" 	539", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("     ", "0123456789abcdef"));
 
   // overflow and undeflow tests
   printf("----- overflow and undeflow tests -----\n");
